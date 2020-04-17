45dfe2efe26a67f074afc053c0af6c4b1c1fe582
HarithWijayawardhana <50038684+HarithWijayawardhana@users.noreply.github.com>
2020-04-17 17:54:13 +0530

    Delete HealthCare.sql

--- DB/HealthCare.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `user` (
-  `userId` int(10) NOT NULL AUTO_INCREMENT,
-  `userName` varchar(50) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `mobile` varchar(10) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `address` varchar(50) NOT NULL,
-  PRIMARY KEY (`userId`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
